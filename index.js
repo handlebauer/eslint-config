b94417f2c18684e9f720824913d2dba9d9b82895
Donald Geddes <hi@donaldgeddes.ca>
2021-12-03 21:45:32 -0800

    Add rules

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  plugins: ['import'],
+  rules: {
+    camelcase: [
+      2,
+      {
+        properties: 'never',
+        ignoreDestructuring: true,
+        ignoreImports: true,
+        ignoreGlobals: true,
+      },
+    ],
+    'consistent-return': 2,
+    'default-case': 2,
+    'no-console': 1,
+    'no-dupe-keys': 2,
+    'no-else-return': 2,
+    'no-undef': 2,
+    'no-unneeded-ternary': 2,
+    'no-unreachable': 2,
+    'no-unused-vars': 2,
+    'object-shorthand': 2,
+    'operator-assignment': 2,
+    'prefer-arrow-callback': 2,
+    'require-await': 2,
+  },
+}
